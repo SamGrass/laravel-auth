e19383c8bc60cd41589c5172002e43c1dc98b343
Samgrass <samuelegrassetti01@gmail.com>
2024-09-24 15:35:10 +0200

    feat: sidebar and header partials for admin page

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,7 @@
+<aside class="text-bg-dark">
+    <ul>
+        <li><a href="{{ route('admin.home') }}">HOME</a></li>
+        <li><a href="#">I miei portfoli</a></li>
+        <li><a href="#">Aggiungi portfolio</a></li>
+    </ul>
+</aside>
